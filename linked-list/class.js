5b25bff5917f3c144f0de8fec706b4c99f88c4a7
gin <73906888+gingkapls@users.noreply.github.com>
2025-03-19 16:17:13 +0530

    feat: add LL class

---
+++ linked-list/class.js
@@ -0,0 +1,50 @@
+import Node from "./Node.js";
+
+class LinkedList {
+  #head;
+  #tail;
+
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+  }
+
+  head() {
+    return this.#head;
+  }
+
+  tail() {
+    return this.#tail;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (this.#head === null) {
+      this.#head = node;
+      this.#tail = this.#head;
+      return;
+    }
+
+    this.#tail.nextNode = node;
+    this.#tail = node;
+  }
+
+  prepend(value) {
+    const node = new Node(value, this.#head);
+    this.#head = node;
+  }
+    
+  size() {
+      let count = 0;
+      let head = this.#head;
+      while (head !== null) {
+        head = head.nextNode;
+        count++
+      }
+      
+      return count;
+  }
+}
+
+export default LinkedList;
