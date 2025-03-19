60b2eb3776974806cde7a36454072430f383072a
gin <73906888+gingkapls@users.noreply.github.com>
2025-03-19 16:25:41 +0530

    feat: add anonymous arrow function expression factory

---
+++ linked-list/anon-arrow-factory.js
@@ -0,0 +1,53 @@
+const Node = require("./Node.js");
+
+const LinkedList = () => {
+  let head = null;
+  let tail = null;
+
+  const append = (value) => {
+    const node = new Node(value);
+    if (head === null) {
+      tail = head = node;
+      return;
+    }
+
+    tail.nextNode = node;
+    tail = node;
+  };
+
+  const prepend = (value) => {
+    const node = new Node(value, head);
+
+    if (head === null) {
+      tail = head = node;
+      return;
+    }
+
+    head = node;
+  };
+
+  const size = () => {
+    let temp = head;
+    let count = 0;
+    while (temp !== null) {
+      temp = temp.nextNode;
+      count++;
+    }
+
+    return count;
+  };
+
+  return {
+    head() {
+      return head;
+    },
+    tail() {
+      return tail;
+    },
+    append,
+    prepend,
+    size,
+  };
+}
+
+module.exports = LinkedList;
